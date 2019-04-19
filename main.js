04951dc3069fc207e0223abeab6686980a078a05
Samson <isamu3636136@gmail.com>
2019-04-19 18:15:52 -0500

    skill page

--- main.js
+++ main.js
@@ -41,16 +41,7 @@ $(window).on('scroll',function(){
     startPos = winScrollTop;
 });
 
-///////// font animation ////////////////////////
-// $(function(){
-// 	$(".subtxt").addClass("changed");
-// 	$(".fullname").addClass("loaded");
-// 	$(".smalltxt").css('opacity', '1');
-// 	$(".downbtn a").css({
-// 		"opacity" : "1",
-// 		"padding" : "15px 10px",
-// 	});
-// });
+///////// font animation ///////////////////////
 
 $(function(){
   // スクロールのファンクション
@@ -66,22 +57,20 @@ $(function(){
       // スクロールして要素が見える時(必要に応じて変更)
       if (scroll > Position - windowHeight + windowHeight/5){
         if($(window).width() < 768) {
-            // 追加のエフェクト(スマホ)
             $(".fadeintxt span",this).css({
               "opacity" : "1",
               "font-size": "52px"
             });
             $(".article", this).css('opacity', '1');
-            $("#skills").addClass("boxfadein");
+            $(".skillContent").addClass("boxfadein");
 
           } else {
-            // 追加のエフェクト(PC)
             $(".fadeintxt span",this).css({
               "opacity" : "1",
               "font-size": "62px"
             });
             $(".article", this).css('opacity', '1');
-            $("#skills").addClass("boxfadein");
+            $(".skillContent").addClass("boxfadein");
 
           }
         } else {
@@ -90,62 +79,15 @@ $(function(){
               "font-size": "0"
             });
             $(".article", this).css('opacity', '0');
-            $("#skills").addClass("boxfadein");
-
-        }
+            $(".skillContent").removeClass("boxfadein");
+      }
     });
   });
 });
 
 
-// スクロールのファンクション
-$(window).scroll(function (){
-  // それぞれのeffectに反映する
-  $(".effect").each(function(){
-    // effectの位置
-    var Position = $(this).offset().top;
-    // スクロールの量
-    var scroll = $(window).scrollTop();
-    // ウィンドウの高さ
-    var windowHeight = $(window).height();
-    // スクロールして要素が見える時(必要に応じて変更)
-    if (scroll > Position - windowHeight + windowHeight/5){
-      if($(window).width() < 768) {
-          // 追加のエフェクト(スマホ)
-          $(".pagefadein",this).css({
-            "opacity" : "1",
-          });
-          $(".introtxt p", this).css('opacity', '1');
-          $("hr", this).css('width', '50%');
-          $("#skills").addClass("boxfadein");
-        } else {
-          // 追加のエフェクト(PC)
-          $(".pagefadein",this).css({
-            "opacity" : "1",
-          });
-          $(".introtxt p", this).css('opacity', '1');
-          $("hr", this).css('width', '50%');
-          $("#skills").addClass("boxfadein");
-        }
-      } else {
-        $(".pagefadein",this).css({
-            "opacity" : "0",
-          });
-          $(".introtxt p", this).css('opacity', '0');
-          $("hr", this).css('width', '0%');
-          $("#skills").addClass("boxfadein");
-        }
-  });
-});
 
-
-
-
-
-
-
-
-
+/////////////////// about page btn ////////////////////
 $(document).ready(function(){
   $("#morebtn").click(function(){
     $("#pro-skill").toggleClass("dropdown");
